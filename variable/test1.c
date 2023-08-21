962cc8cef981416f1e828f8017f4c09da3fa1a97
tzyrrxr <sjlin0827@outlook.com>
2023-08-21 21:47:32 +0800

    update test

---
+++ variable/test1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+ printf(">>>>>>> test1.c\n");
+ printf(">>>>>>> test1.c\n");
+ return 0;
+}
---
+++ variable/test2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+ printf(">>>>>> test2.c\n");
+ return 0;
+}
